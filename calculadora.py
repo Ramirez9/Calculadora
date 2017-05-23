75c2722437d78d6ae97f3f4abd327127b6258bd9
dawagja <jose.antonio.aguilar.granados@gmail.com>
2017-05-23 15:17:02 +0200

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -10,7 +10,7 @@ while True:
 -------------MENU CALCULADORA---------------
  
 Pulse 1 para sumar dos dígitos
-Pulse 2 para restar dos dígito
+Pulse 2 para restar dos dígitosdfghjklñlkjhgfd
 Pulse 3 para multiplicar dos dígitos
 Pulse 4 para dividir dos dígitos 
 Pulse 5 para salir del progama
@@ -19,15 +19,15 @@ Pulse 5 para salir del progama
     opcion=input("Que número desea: ")
     
     if opcion == 1:
-        print sumar(a=input("Introduzca el primer numero: "), b=input("Introduzca el segundo numero: "))
+        print sumar(a=input("Introduzca el prkjhgfdsimer numero: "), b=input("Introduzca el segundo numero: "))
     elif opcion == 2:
-        print restar(a=input("Introduzca el primer numero: "), b=input("Introduzca el segundo numero: "))
+        print restar(a=input("Introduzca el primer nugfdsmero: "), b=input("Introduzca el segundo numero: "))
  
     elif opcion == 3:
-        print multiplicar(a=input("Introduzca el primer numero: "), b=input("Introduzca el segundo numero: "))
+        print multiplicar(a=input("Introduzca el primerewqrtryu numero: "), b=input("Introduzca el segu65yu5terndo numero: "))
  
     elif opcion == 4:
-        print dividir(a=input("Introduzca el primer numero: "), b=input("Introduzca el segundo numero: "))
+        print dividir(a=input("Introduztrw4tca el primer numero: "), b=input("Introduzca el seguefgrhndo numero: "))
         
     elif opcion == 5:
         print "Hasta luego"
